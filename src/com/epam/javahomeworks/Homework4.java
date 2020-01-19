97a2bc5a012811e6ae828e6054934dff6dbe3230
DmitriiSvnk <savenko.dmtr@gmail.com>
2020-01-19 21:14:29 +0200

    Update Homework4.java

--- src/com/epam/javahomeworks/Homework4.java
+++ src/com/epam/javahomeworks/Homework4.java
@@ -3,33 +3,33 @@ package com.epam.javahomeworks;
 import java.util. Scanner;
 
 public class Homework4 {
+//  Task 1
+    static String getUniqueSymbols(String text) {
+        String textWithoutSpaces = text.replaceAll(" ", "");
+        String result = "";
+        int quantitySymbols = textWithoutSpaces.length();
+        int counter = 0;
+        do {
+            if (result.indexOf(textWithoutSpaces.charAt(counter)) == -1) {
+                result += (textWithoutSpaces.charAt(counter) + ", ");
+            }
+            counter++;
+        } while (counter < quantitySymbols);
+        return result;
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Введите текст: ");
         String text = input.nextLine();
-        int quantitySymbols = (text.length() - 1);
-        String uniqueSymbols = " ";
-        int quantityUniqueSymbols = (uniqueSymbols.length() - 1);
-        int symbolNumber;
-        Character symbol;
-        Character a;
-        int counter = 0;
-        while (counter <= quantitySymbols) {
-            symbolNumber = 0;
-            do {
-                if (symbolNumber <= quantityUniqueSymbols) {
-                    symbol = text.charAt(counter);
-                    a = uniqueSymbols.charAt(symbolNumber);
-                    if (symbol.equals(a)) {
-                    } break;
-                } else {
-                    uniqueSymbols = uniqueSymbols + symbol + ", ";
-                }
-                symbolNumber++;
-            } while (symbolNumber <= counter);
-            counter++;
-        }
-        System.out.println("Символы из которых состоит текст:");
-        System.out.print(uniqueSymbols);
+        String uniqueSymbols = getUniqueSymbols(text);
+        System.out.println("Символы из которых состоит текст: " + uniqueSymbols);
+
+//  Task 2
+        int number;
+        System.out.println("Введите целое десятичное число: ");
+        number = input.nextInt();
+        String convert = Integer.toBinaryString(number);
+        System.out.println("Ваше число в двоичной системе счисления: " + convert);
     }
 }
