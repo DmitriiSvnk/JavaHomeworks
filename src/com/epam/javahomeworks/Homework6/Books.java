d884f4b5a5febb7941bdd672e95a9c1c5954ada1
DmitriiSvnk <savenko.dmtr@gmail.com>
2020-01-30 13:54:56 +0200

    Update Books.java

--- src/com/epam/javahomeworks/Homework6/Books.java
+++ src/com/epam/javahomeworks/Homework6/Books.java
@@ -4,13 +4,9 @@ import  java.util.Arrays;
 
 public class Books {
     private Book[] books;
-
-    public Books(int size) {
-        if (size >= 0) {
-            books = new Book[size];
-        } else {
-            System.err.println("Not found.");
-        }
+    private int counter;
+    public Books() {
+        books = new Book[0];
     }
 
     public Book[] getBooks() {
@@ -21,13 +17,12 @@ public class Books {
     }
 
     public void addBook(Book book) {
-        int size = books.length + 1;
-        books = Arrays.copyOf(books, size);
-        books[--size] = book;
+        books = Arrays.copyOf(books, books.length + 1);
+        books[counter++] = book;
     }
     public void viewAllBooks() {
         if (books.length == 0) {
-            System.out.println("Nothing found");
+            System.out.println("Not found");
         } else {
             for (Book book : books) {
                 book.viewBook();
@@ -36,31 +31,31 @@ public class Books {
     }
     public void increasePrise(int percent) {
         for (Book book : books) {
-            int oldPrice = book.getPrice();
-            int margin = (oldPrice / 100) * percent;
-            int newPrice = oldPrice + margin;
+            double oldPrice = book.getPrice();
+            double margin = (oldPrice / 100) * percent;
+            double newPrice = oldPrice + margin;
             book.setPrice(newPrice);
         }
     }
     public void decreasePrise(int percent) {
         for (Book book : books) {
-            int oldPrice = book.getPrice();
-            int margin = (percent / 100) * oldPrice;
-            int newPrice = oldPrice - margin;
+            double oldPrice = book.getPrice();
+            double margin = (oldPrice / 100) * percent;
+            double newPrice = oldPrice - margin;
             book.setPrice(newPrice);
         }
     }
     public Books searchByAuthor(String author) {
-        Books selectedBooks = new Books(0);
+        Books selectedBooks = new Books();
         for (Book book : books) {
-            if (book.getAuthor().equals(author)) {
+            if (book.getAuthor().equalsIgnoreCase(author)) {
                 selectedBooks.addBook(book);
             }
         }
         return selectedBooks;
     }
     public Books searchByYear(int year) {
-        Books selectedBooks = new Books(0);
+        Books selectedBooks = new Books();
         for (Book book : books) {
             if (book.getYear() > year) {
                 selectedBooks.addBook(book);
